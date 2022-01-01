943acf436027044302fa81bbfc5e5bd5359a3c1d
xh <xh980103@163.com>
2022-01-01 22:44:21 +0800

    feat: 添加登录状态维持

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -4,6 +4,7 @@ import { SearchPanel } from "./search-panel";
 import { useState, useEffect } from "react";
 import { cleanObject, useMount, useDebounce } from "utils/index";
 import * as qs from "qs";
+import { useHttp } from "utils/http";
 
 const apiUrl = process.env.REACT_APP_API_URL;
 
@@ -22,22 +23,14 @@ export const ProjectListScreen = () => {
 
   const debounceParam = useDebounce(param, 500);
 
+  const client = useHttp();
+
   useEffect(() => {
-    fetch(
-      `${apiUrl}/projects?${qs.stringify(cleanObject(debounceParam))}`
-    ).then(async (response) => {
-      if (response.ok) {
-        setList(await response.json());
-      }
-    });
+    client("projects", { data: cleanObject(debounceParam) }).then(setList);
   }, [debounceParam]);
 
   useMount(() => {
-    fetch(`${apiUrl}/users`).then(async (response) => {
-      if (response.ok) {
-        setUsers(await response.json());
-      }
-    });
+    client("users").then(setUsers);
   });
 
   return (
